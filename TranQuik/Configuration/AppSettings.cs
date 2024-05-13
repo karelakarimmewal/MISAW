98f52671fec6674064b1de5e0b47657b12663f09
karelakarimmewal <karelmewal.17@gmail.com>
2024-05-13 08:44:34 +0700

    First Commit 051324

---
+++ TranQuik/Configuration/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace TranQuik.Configuration
+{
+    public static class AppSettings
+    {
+        public static int AppFontSize { get; set; }
+        public static string AppFontFamily { get; set; }
+        public static int AppSaleMode { get; set; }
+        public static string AppID { get; set; }
+        public static bool AppSecMonitor { get; set; }
+        public static bool AppAllowImage { get; set; }
+        public static bool AppStatus { get; set; }
+    }
+}
---
+++ TranQuik/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using TranQuik.Configuration;
+
+namespace TranQuik
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Config.LoadAppSettings();
+        }
+    }
+}
